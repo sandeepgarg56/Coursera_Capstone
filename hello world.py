961972b8beb80235af679e1fe2bdd87d6b227f6f
sandeepgarg56 <61867307+sandeepgarg56@users.noreply.github.com>
2020-04-29 02:15:40 +0530

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  that this notebook will be mainly used for the capstone project which is titled as coursera_capstone. it is  for basic understanding of pushing a code in github.
+#  
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
